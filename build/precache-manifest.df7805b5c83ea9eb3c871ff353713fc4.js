9f012b5a0fcb4b10177106023c8971dd7dc9c77b
sevskii <sevskii111@gmail.com>
2019-12-24 19:29:47 +0300

    added build to commit

---
+++ build/precache-manifest.df7805b5c83ea9eb3c871ff353713fc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f6821b7abbe8eebe54683395beb121",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63cfbd2c414e3783cebd",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3c38901132113ee3e0aa",
+    "url": "/static/css/main.bb75355a.chunk.css"
+  },
+  {
+    "revision": "63cfbd2c414e3783cebd",
+    "url": "/static/js/2.86ea186f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.86ea186f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c38901132113ee3e0aa",
+    "url": "/static/js/main.e2ce615d.chunk.js"
+  },
+  {
+    "revision": "d8fea4164ef84402df16",
+    "url": "/static/js/runtime-main.6e603ebc.js"
+  }
+]);
